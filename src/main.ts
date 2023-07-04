075b77956b30c36821231b9b7c5f10c00e97d800
trunglemobiledev <lmt.cn.tv@gmail.com>
2023-07-04 13:09:03 +0700

    Install swaggger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Base api')
+    .setDescription('API document')
+    .setVersion('1.0')
+    .addTag('blogs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
